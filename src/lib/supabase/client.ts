19d1825bb9c4bcc5b137b12d4a3008176b9638d4
gustavo.sena <gustavo.sena@onlyhighiq.com>
2025-12-18 22:37:04 +0000

    Revert unecessary change due to env var missmatch

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,15 +1,11 @@
-import { createClient, type SupabaseClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase: SupabaseClient =
-  typeof window === "undefined"
-    ? ({} as SupabaseClient)
-    : createClient(supabaseUrl!, supabaseAnonKey!);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export async function getSession() {
-  if (typeof window === "undefined") return null;
   const { data, error } = await supabase.auth.getSession();
   if (error) {
     console.error("Error getting session:", error);
@@ -19,7 +15,6 @@ export async function getSession() {
 }
 
 export async function getUser() {
-  if (typeof window === "undefined") return null;
   const { data, error } = await supabase.auth.getUser();
   if (error) {
     console.error("Error getting user:", error);
